8bdaa2f94183ebfc5313592d7fdbfe7f0113d54b
Abhinav Varshney <abhihts20@gmail.com>
2019-06-13 11:45:54 +0530

    Login App Initial Commit

--- app/src/main/java/com/appsquadz/loginapp/MainActivity.java
+++ app/src/main/java/com/appsquadz/loginapp/MainActivity.java
@@ -1,13 +1,26 @@
 package com.appsquadz.loginapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Context contextmain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        contextmain=this;
+        TextView textview=findViewById(R.id.txt_01);
+        textview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(contextmain,SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
